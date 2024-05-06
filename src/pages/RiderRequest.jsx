f99605ba1b1ece65cd01a2d706dbdf0a57d4db8a
Jefrint <jefrintm@gmail.com>
2024-05-06 10:11:40 +0530

    suggestion card updated with firebase code

--- src/pages/RiderRequest.jsx
+++ src/pages/RiderRequest.jsx
@@ -15,7 +15,7 @@ const RiderRequest = () => {
 
     return (
         <div className="flex justify-center items-center h-screen m-5">
-            <div className="bg-white shadow-lg rounded-lg bg-gray-100 w-full max-w-md p-8">
+            <div className=" shadow-lg rounded-lg bg-gray-100 w-full max-w-md p-8">
                 <div className="bg-gray-200 rounded-lg p-4 mb-6 flex flex-col items-center">
                     <img src={img} alt="Profile" className="w-16 h-16 rounded-full mb-2" /> {/* Photo */}
                     <p className="text-xl font-semibold">{name} Kuriakose</p> {/* Name */}
